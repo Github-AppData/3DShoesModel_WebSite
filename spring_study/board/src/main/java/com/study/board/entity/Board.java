e01e7b8f08141b193d08035df95cb04b32d4e95b
Erani <xodnr2019@naver.com>
2023-10-03 21:14:42 +0900

    asda

--- spring_study/board/src/main/java/com/study/board/entity/Board.java
+++ spring_study/board/src/main/java/com/study/board/entity/Board.java
@@ -8,7 +8,6 @@ import javax.persistence.Id;
 import lombok.Data;
 
 @Entity
-@Data
 public class Board {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +20,52 @@ public class Board {
 	private String filename;
 	
 	private String filepath;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public String getFilepath() {
+		return filepath;
+	}
+
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+
+	@Override
+	public String toString() {
+		return "Board [id=" + id + ", title=" + title + ", content=" + content + ", filename=" + filename
+				+ ", filepath=" + filepath + "]";
+	}
+	
+	
 }
