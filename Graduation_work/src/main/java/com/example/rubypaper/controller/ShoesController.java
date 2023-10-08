119c636b4ba6df909b01cfaa6bdb4939433ca80b
pjhyun01 <100830249+pjhyun01@users.noreply.github.com>
2023-10-08 19:26:47 +0900

    TEST : 병합 오류 해결 중

---
+++ Graduation_work/src/main/java/com/example/rubypaper/controller/ShoesController.java
@@ -0,0 +1,44 @@
+package com.example.rubypaper.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.rubypaper.dto.Shoes;
+import com.example.rubypaper.service.ShoesService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ShoesController {
+	
+	@Autowired
+	ShoesService shoesService;
+	
+	@PostMapping("/test_Search")
+	public String searchTest(@ModelAttribute Shoes shoes, Model model) throws Exception{
+		List<Shoes> shoesList = shoesService.Search(shoes);
+		
+		if(shoesList != null){
+			model.addAttribute("result",shoesList);
+			return "test/test_Search";
+		}
+		else {
+			return "test/sMain";
+		}
+		/*
+		System.out.println(shoesList.size());
+		for(int i = 0; i < shoesList.size(); i++)
+		{
+			System.out.println(shoesList.get(i).toString());
+		}
+		*/
+		//System.out.println(shoesList);
+	}
+}
