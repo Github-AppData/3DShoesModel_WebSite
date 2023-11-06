98da20f6b0ffe2662562036c8b90f4727f6962c5
Github-AppData <xodnr2019@naver.com>
2023-11-06 16:17:24 +0900

    All Remove

--- springboot/jpa/src/main/java/com/practice/jpa/Repository/MemoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.practice.jpa.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.practice.jpa.Entity.Memo;
-
-public interface MemoRepository extends JpaRepository<Memo, Long> {
-}
